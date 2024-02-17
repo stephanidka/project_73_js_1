6e10311aa6f3ab216b2b41a237da147d2bda06b3
Sunkiti <elena.bulavkina@gmail.com>
2024-02-17 16:58:11 +0300

    new js

--- assets/script/script.js
+++ assets/script/script.js
@@ -254,12 +254,19 @@ const fetchFiltrMovies = async (
   countrie,
   genres,
   page = 1,
-  limit = 10
+  limit = 9
 ) => {
-  const url = `https://api.kinopoisk.dev/v1.4/movie?page=${page}&limit=${limit}&selectFields=countries&selectFields=description&selectFields=name&selectFields=genres&selectFields=poster&selectFields=type&selectedFields=shortDescription&selectFields=year&notNullFields=id&year=${year}&genres.name=${genres}&countries.name=${countrie}`;
+  const switcherMovieSeries = document.getElementById("switch") // это переключатель с фильмов на сериалы
+  let responseURL;
+  if (switcherMovieSeries.checked) {
+    responseURL = `https://api.kinopoisk.dev/v1.4/movie?type=tv-series&&animated-series&page=${page}&limit=${limit}&selectFields=countries&selectFields=description&selectFields=name&selectFields=genres&selectFields=poster&selectFields=type&selectFields=shortDescription&selectFields=year&notNullFields=id&year=${year}&genres.name=${genres}&countries.name=${countrie}`;
+  } else {
+    responseURL = `https://api.kinopoisk.dev/v1.4/movie?type=movie&&cartoon&&anime&page=${page}&limit=${limit}&selectFields=countries&selectFields=description&selectFields=name&selectFields=genres&selectFields=poster&selectFields=type&selectFields=shortDescription&selectFields=year&notNullFields=id&year=${year}&genres.name=${genres}&countries.name=${countrie}`;
+  }
+  const url = responseURL;
   const headers = {
     accept: "application/json",
-    "X-API-KEY": API_KEY,
+    "X-API-KEY": token,
   };
   const response = await fetch(url, { headers });
   const data = await response.json();
@@ -269,25 +276,25 @@ const fetchFilteredMovies = async () => {
   const yearSelect = document.getElementById('years-select').value;
   const countrySelect = document.getElementById('country_select').value;
   const genreCheckboxes = Array.from(document.querySelectorAll('.container-input__tag[name="genre"]:checked')).map(checkbox => checkbox.value);
-
   const res = await fetchFiltrMovies(yearSelect, countrySelect, genreCheckboxes);
   console.log(res);
-
-  const movie = res.docs[0]; // это добавление картики в блок search-results
-  console.log(movie);
-  const postMovies = document.querySelector(".search-results");
-  const generateMovieHTML = (movie) => {
-    return `
+  const postMovies = document.querySelector(".search-results__conteiner");
+  for (let i = 0; i <= res.docs.length; i++) {
+    const movie = res.docs[i];
+    console.log(movie);
+    const generateMovieHTML = (movie) => {
+      return `
         <div class="post">
-        <img src="${movie.poster.url}" alt="${movie.name} Poster">
-            <p>${movie.name}</p>
-            <p>${movie.countries.map(country => country.name).join(', ')}</p>
-            <p>${movie.year}</p>
+        <img class="search-results__img" src="${movie.poster.url}" alt="${movie.name}">
+            <p class="search-results__name">${movie.name}</p>
+            <p class="search-results__par">${movie.countries.map(country => country.name).join(', ')}</p>
+            <p class="search-results__par">${movie.year}</p>
+            <button class="btn__add" type="button">Add to my film list</button>
         </div>`;
-  };
-  const movieHTML = generateMovieHTML(movie);
-  postMovies.innerHTML += movieHTML; // всё, добавлена
-
+    };
+    const movieHTML = generateMovieHTML(movie);
+    postMovies.innerHTML += movieHTML; // всё, добавлена
+  }
 };
 document.querySelector('.section-search__glow-on-hover').addEventListener('click', fetchFilteredMovies);
 
@@ -379,6 +386,46 @@ document.querySelector('.section-search__glow-on-hover').addEventListener('click
 
 
 // Lena
+// const switcher = document.querySelector(".section-search__label");
+// console.log(switcher);
+// let seriesArray = []; //series array
+
+// function getSeries() {
+//   let checkbox = document.getElementById("switch");
+//   if (checkbox.checked == true) {
+//     const fetchFiltrSeries = async (
+//       year,
+//       countrie,
+//       genres,
+//       page = 1,
+//       limit = 10,
+//       type = tv - series,
+//     ) => {
+//       const url = `https://api.kinopoisk.dev/v1.4/movie?page=${page}&limit=${limit}&selectFields=countries&selectFields=description&selectFields=name&selectFields=genres&selectFields=poster&selectFields=shortDescription&selectFields=year&notNullFields=id&year=${year}&genres.name=${genres}&countries.name=${countrie}$type.name=${tv - series}`;
+//       const titles = {
+//         accept: "application/json",
+//         "X-API-KEY": token,
+//       };
+//       const response = await fetch(url, { titles });
+//       const data = await response.json();
+//       return data;
+//     };
+//     const fetchFilteredSeries = async () => {
+//       const series = Array.type[tv - series];
+//       const result = await fetchFiltrSeries;
+//       console.log(result);
+//     }
+//   }
+// };
+
+//     }
+// console.log(checkbox);
+// let output = [];
+// checkbox.forEach((checkbox) => {
+//     output.push(checkbox.value);
+//     // });
+//     // seriesArray = output;
+// }
 
 const submit = document.getElementById("submit");
 function subscribeCheckValidity(e) {
